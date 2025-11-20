13a4a9b6f93fbf2b325f5a823a48c837577ce274
mochinekoo <mochinekoo0113@gmail.com>
2025-11-20 23:03:05 +0900

    [add] CPUとメモリ使用率を出力できるように

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
 import sys
+import time
+import psutil
 
 from SubClass import SubClass
 
 subClass = SubClass()
 number_list = [10, 20, 30, 40]
+
+cpu_percent = psutil.cpu_percent(percpu=True)
+mem = psutil.virtual_memory()
 
 subClass.print("てすと")
 print("入出力を受け付けてます: \n")
@@ -15,4 +20,9 @@ while True:
         sys.exit(0)
     elif i.lower() == 'number_list':
         print("3番目の要素は：" + str(number_list[3]) + "です")
+    elif i.lower() == 'cpumem_info':
+        time.sleep(2)
+        cpu_percent = psutil.cpu_percent(percpu=True)
+        print("CPU", cpu_percent)
+        print("Mem", mem.percent)
     print("\n")
