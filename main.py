7fd908d6f514908cafb77591c13925a1be8a6586
mochinekoo <mochinekoo0113@gmail.com>
2025-11-20 22:44:19 +0900

    [add] 入出力処理を学びました

--- main.py
+++ main.py
@@ -1,6 +1,15 @@
+import sys
+
 from SubClass import SubClass
 
 subClass = SubClass()
 
 subClass.print("てすと")
-print("HelloWorld")
+print("入出力を受け付けてます: \n")
+
+while True:
+    i = input()
+    if i.lower() == 'exit':
+        print("終了します")
+        sys.exit(0)
+    print("\n")
