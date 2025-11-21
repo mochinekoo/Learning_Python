dc779b1a9fcf39b64403b18a10c577ef0c8ff925
mochinekoo <mochinekoo0113@gmail.com>
2025-11-21 13:05:29 +0900

    [add] コメントとドキュメントについて学びました

--- CommandBase.py
+++ CommandBase.py
@@ -1,11 +1,18 @@
 from abc import abstractmethod, ABC, ABCMeta
 
-
+#　これはコメントです
 class CommandBase(ABC, metaclass=ABCMeta):
+    """ コマンド用のクラス
+    """
 
     def __init__(self):
         pass
 
     @abstractmethod
     def runcmd(self, name):
+        """ コマンドを実行する関数
+        :param name: 実行したいコマンド名
+        :return: 実行に成功した場合はTrue、失敗した場合はFalseを返す
+        """
+
         pass
